eb5a10765fbee8a628e26677d22676ef2ddfaafc
James Errico <james.errico@gmail.com>
2013-02-10 23:53:41 -0500

    Update setup.sh
    Corrected mistakes, added shebang

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 mkdir ~/.vim
 mkdir ~/.vim/backups
 mkdir ~/.vim/swaps
