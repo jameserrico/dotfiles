67a33bd4f68d74824ddfddcd30682e29df8be12a
James <jerrico@fortuna.(none)>
2013-02-11 12:38:54 -0500

    Add vimrc configs, made it so symbolic links in setup.h force overwrite

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 mkdir ~/.vim
 mkdir ~/.vim/backups
 mkdir ~/.vim/swaps
-ln -s ~/dotfiles/virtualenvwrapper/postactivate ~/.virtualenvs/postactivate
-ln -s ~/dotfiles/virtualenvwrapper/postdeactivate ~/.virtualenvs/postdeactivate 
+ln -sf ~/dotfiles/virtualenvwrapper/postactivate ~/.virtualenvs/postactivate
+ln -sf ~/dotfiles/virtualenvwrapper/postdeactivate ~/.virtualenvs/postdeactivate 
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,5 +1,30 @@
+set nomodeline
 set nocompatible
-set number
-
 set backupdir=~/.vim/backups
 set directory=~/.vim/swaps
+
+set number
+set linebreak
+set showbreak=+++
+set textwidth=100
+set showmatch	
+set errorbells
+set visualbell
+ 
+set hlsearch
+set smartcase
+set ignorecase
+set incsearch
+ 
+set autoindent
+set shiftwidth=4	
+set smartindent
+set smarttab
+set softtabstop=4	
+ 
+set ruler	
+ 
+set undolevels=1000
+set backspace=indent,eol,start	
+
+
