d11d86af84a2aaef679f01e1b64249b703dab563
James Errico <james.errico@gmail.com>
2013-02-10 23:51:03 -0500

    Initial checkin

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/backups
+mkdir ~/.vim/swaps
---
+++ vim/.vimrc
@@ -0,0 +1,5 @@
+set nocompatible
+set number
+
+set backupdir=~/.vim/backups
+set directory=~/.vim/swaps
